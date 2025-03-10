c711ed9e727e547b5f684af8bf43a9b7e8b4f4f3
Ivan Sigaev <ivan.sigaev@check24.de>
2025-03-10 22:11:25 +0100

    feat: add makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -131,8 +131,15 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  return Object.entries(lettersObject)
+    .reduce((acc, [key, value]) => {
+      value.forEach((index) => {
+        acc[index] = key;
+      });
+      return acc;
+    }, [])
+    .join('');
 }
 
 /**
